79e9972b10f93e26f140b87070189d2c73f4d616
Dylan Ryan <dylanryan431@gmail.com>
2024-04-03 23:32:46 -0300

    added credits

---
+++ Assets/Scripts/Credits.cs
@@ -0,0 +1,6 @@
+https://pixabay.com/sound-effects/blocking-arm-with-hand-6941/
+https://pixabay.com/sound-effects/space-ship-door-open-47688/
+https://pixabay.com/sound-effects/transition-futuristic-teleport-121420/
+https://pixabay.com/sound-effects/elevator-29654/
+https://pixabay.com/sound-effects/transition-futuristic-teleport-121420/
+https://pixabay.com/sound-effects/coin-pickup-98269/
